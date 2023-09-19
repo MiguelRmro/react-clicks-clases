dcf52c90114c2582f3445f109365eb9c9701a55c
Miguel Romero <mr2004romero@gmail.com>
2023-09-19 07:34:58 -0600

    react-clicks-clases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numClics: 0  
+    };
+    this.manejarClic = this.manejarClic.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
+  }
+
+  manejarClic(){
+    this.setState(({ numClics }) => ({ numClics: numClics +1 }));
+  }
+
+  reiniciarContador(){
+    this.setState ({ numClics: 0 });
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <div className='freecodecamp-logo-contenedor'> 
+        <img 
+            className='freecodecamp-logo'
+            src={freeCodeCampLogo}
+            alt='Logo de freeCodeCamp' 
+        />
+        </div>
+         <div className='contenedor-principal'>
+          <Contador numClics={this.state.numClics} />
+          <Boton
+          texto='Click'
+          esBotonDeClic={true}
+          manejarClic={this.manejarClic} />
+          <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={this.reiniciarContador} />
+         </div>
+      </div>
+    );
+  }
+}
+
+/*function App() { 
+
+  const [numClics, setNumClics] = useState(0)
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1) ;
+  }
+
+  const reiniciarContador = () => [
+    setNumClics(0)
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'> 
+      <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp' 
+      />
+      </div>
+       <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+        texto='Click'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton
+        texto='Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}/>
+       </div>
     </div>
   );
 }
-
+*/
 export default App;
---
+++ src/componentes/Boton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../hojas-de-estilo/boton.css';
+
+class Boton extends React.Component {
+  render() {
+    return (
+      <button
+      className={this.props.esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+      onClick={this.props.manejarClic}>
+        {this.props.texto}
+      </button>
+    );
+  }
+}
+
+export default Boton;
